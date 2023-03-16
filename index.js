ac3f7961542e3524afc1af02f12982a10bcc026f
JeanMMillet <millet.jm@live.com>
2023-03-16 12:01:53 +0100

    Finis

---
+++ index.js
@@ -0,0 +1,7 @@
+
+import chalk from "chalk";
+
+console.log(chalk.greenBright("Jean-Maxime"));
+console.log(chalk.red("Etienne"));
+console.log(chalk.blue("Benjamin"));
+console.log(chalk.yellow("Julien"));
